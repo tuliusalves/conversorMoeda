b8d2b891c505923540db4419e97a8ec988f6e8bc
Túlius <tulius_sr@outlook.com>
2024-05-01 13:06:19 -0300

    Implementando teste de requisição de API

--- ConversorDeMoeda/src/Main.java
+++ ConversorDeMoeda/src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import com.google.gson.Gson;
 
-        System.out.println("Hello world!");
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/059c5e45ced2397a6b06506b/latest/USD"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
